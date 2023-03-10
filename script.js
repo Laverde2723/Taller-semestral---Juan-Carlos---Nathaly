69d9d30077cec2899f4b81d8cd82ed16051e8568
NathMfx <nathalymartinezdiseno@gmail.com>
2023-03-09 19:46:10 -0500

    por finn

---
+++ script.js
@@ -0,0 +1,34 @@
+let categoria1 = document.getElementById("categoria1")
+
+categoria1.addEventListener("mouseover", e=>{
+    categoria1.src="./Imag/modelo1-2.png"
+
+})
+
+categoria1.addEventListener("mouseleave", e=>{
+
+    categoria1.src="./Imag/modelo1.png"
+    
+})
+
+categoria2.addEventListener("mouseover", e=>{
+    categoria2.src="./Imag/modelo2-1.jpg"
+
+})
+
+categoria2.addEventListener("mouseleave", e=>{
+
+    categoria2.src="./Imag/modelo.2.png"
+    
+})
+
+categoria3.addEventListener("mouseover", e=>{
+    categoria3.src="./Imag/modelo3-1.jpg"
+
+})
+
+categoria3.addEventListener("mouseleave", e=>{
+
+    categoria3.src="./Imag/modelo3.jpg"
+    
+})
